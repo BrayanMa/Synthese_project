9f239963fd0e862d6e6807673daec2f0ca3e2ff6
BrayanMa <mariebrayan724@gmail.com>
2022-04-06 17:49:35 +0200

    Fin de la première scène

--- src/scene.c
+++ src/scene.c
@@ -9,25 +9,49 @@ Shape *cube3;
 Shape *cube4;
 Shape *cube5;
 Shape *cube6;
+Shape *cube7;
+Shape *cube8;
+Shape *cube9;
+Shape *cube10;
+Shape *cube11;
+Shape *cube12;
+Shape *cube13;
+
+Shape *cube14;
+
 Shape *cylinder1;
 Shape *cylinder2;
 Shape *cylinder3;
 Shape *cylinder4;
 Shape *cylinder5;
-Shape *test;
+Shape *cylinder6;
+Shape *cylinder7;
+Shape *cylinder8;
+Shape *cylinder9;
+Shape *cylinder10;
 
 Node *full_scene;
 
 Node *table;
+Node *table2;
+
 Node *table_ronde;
+Node *table_ronde2;
 
 Node *sol;
+Node *sol2;
+Node *sol3;
+Node *sol4;
 
 Node *plateau;
 Node *plateau2;
+Node *plateau3;
+Node *plateau4;
 
 Node *pieds;
 Node *pieds2;
+Node *pieds3;
+Node *pieds4;
 
 Node *pied1;
 Node *pied2;
@@ -39,7 +63,15 @@ Node *pied6;
 Node *pied7;
 Node *pied8;
 
-Node *test1;
+Node *pied9;
+Node *pied10;
+Node *pied11;
+Node *pied12;
+
+Node *pied13;
+Node *pied14;
+Node *pied15;
+Node *pied16;
 
 double mat[4] = {.2, .6, .9, 1};
 
@@ -53,23 +85,34 @@ static void init(void)
   cube4 = init_cube();
   cube5 = init_cube();
   cube6 = init_cube();
-  test = init_cylinder();
+  cube7 = init_cube();
+  cube8 = init_cube();
+  cube9 = init_cube();
+  cube10 = init_cube();
+  cube11 = init_cube();
+  cube12 = init_cube();
+  cube13 = init_cube();
+  cube14 = init_cube();
 
   cylinder1 = init_cylinder();
   cylinder2 = init_cylinder();
   cylinder3 = init_cylinder();
   cylinder4 = init_cylinder();
   cylinder5 = init_cylinder();
-
-  test1 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.3, 0.3, 0.3, 0.00}, mat, (G3Xvector){1, 1, 1}, test);
-  apply_homot(test1, .5, .5, 1);
+  cylinder6 = init_cylinder();
+  cylinder7 = init_cylinder();
+  cylinder8 = init_cylinder();
+  cylinder9 = init_cylinder();
+  cylinder10 = init_cylinder();
 
   full_scene = init_node(g3x_Identity(), (G3Xcolor){0.3, 0.3, 0.3, 0.00}, mat, (G3Xvector){1, 1, 1});
+  apply_homot(full_scene, .7, .7, .7);
+  apply_trans(full_scene, .5, .5, 0);
 
   /*Premier element */
   sol = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.3, 0.3, 0.3, 0.00}, mat, full_scene->scale_factor, cube2);
   apply_homot(sol, 1.3, 1.3, .01);
-  apply_rotat(sol, 0, 0, 1);
+  // apply_rotat(sol, 0, 0, 1);
 
   /* Table normale */
   table = init_node(g3x_Identity(), (G3Xcolor){0.30, 0.20, 0.10, 0.00}, mat, full_scene->scale_factor);
@@ -110,9 +153,67 @@ static void init(void)
   pied8 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, pieds2->scale_factor, cube6);
   apply_trans(pied8, 0, -13, -1);
 
+  /* Deuxieme element */
+  sol2 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.6, 0.6, 0.6, 0.00}, mat, full_scene->scale_factor, cube7);
+  apply_homot(sol2, 1.3, 1.3, .01);
+  // apply_rotat(sol2, 0, 0, 1);
+  apply_trans(sol2, 0, -2.6, 0);
+
+  /*Table ronde*/
+  table_ronde2 = init_node(g3x_Identity(), (G3Xcolor){0.40, 0.30, 0.20, 0.00}, mat, full_scene->scale_factor);
+  apply_trans(table_ronde2, 0, -2.6, 1);
+
+  plateau3 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.60, 0.40, 0.20, 0.00}, mat, table_ronde->scale_factor, cylinder6);
+  apply_homot(plateau3, .5, .5, .05);
+
+  pieds3 = init_node(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, table_ronde2->scale_factor);
+  apply_homot(pieds3, .03, .03, .5);
+
+  pied9 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, pieds3->scale_factor, cube8);
+  apply_trans(pied9, -13, 0, -1);
+  pied10 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, pieds3->scale_factor, cube9);
+  apply_trans(pied10, 13, 0, -1);
+  pied11 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, pieds3->scale_factor, cube10);
+  apply_trans(pied11, 0, 13, -1);
+  pied12 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, pieds3->scale_factor, cube11);
+  apply_trans(pied12, 0, -13, -1);
+
+  /*Troisième élément*/
+  sol3 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.3, 0.3, 0.3, 0.00}, mat, full_scene->scale_factor, cube12);
+  apply_homot(sol3, 1.3, 1.3, .01);
+  apply_trans(sol3, -2.6, -2.6, 0);
+
+  /*Quatrième sol*/
+  sol4 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.6, 0.6, 0.6, 0.00}, mat, full_scene->scale_factor, cube13);
+  apply_homot(sol4, 1.3, 1.3, .01);
+  apply_trans(sol4, -2.6, 0, 0);
+
+  /* Deuxième table normale */
+  table2 = init_node(g3x_Identity(), (G3Xcolor){0.30, 0.20, 0.10, 0.00}, mat, full_scene->scale_factor);
+  apply_trans(table2, -2.6, -1, 1);
+  apply_rotat(table2, 0, 0, -.3);
+
+  plateau4 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.30, 0.20, 0.10, 0.00}, mat, table2->scale_factor, cube14);
+  apply_homot(plateau4, .5, 1, .05);
+
+  pieds4 = init_node(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, table2->scale_factor);
+  apply_homot(pieds4, .05, .05, .5);
+
+  pied13 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, pieds4->scale_factor, cylinder6);
+  apply_trans(pied13, 8, -18, -1);
+  pied14 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, pieds4->scale_factor, cylinder8);
+  apply_trans(pied14, 8, 18, -1);
+  pied15 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, pieds4->scale_factor, cylinder9);
+  apply_trans(pied15, -8, -18, -1);
+  pied16 = init_node_with_shape(g3x_Identity(), (G3Xcolor){0.60, 0.75, 0.95, 0.00}, mat, pieds4->scale_factor, cylinder10);
+  apply_trans(pied16, -8, 18, -1);
+
   /*Lien*/
   full_scene->down = sol;
-  sol->next = table;
+  sol->next = sol2;
+  sol2->next = sol3;
+  sol3->next = sol4;
+  sol4->next = table;
   table->down = plateau;
   plateau->next = pieds;
   pieds->down = pied1;
@@ -126,6 +227,20 @@ static void init(void)
   pied5->next = pied6;
   pied6->next = pied7;
   pied7->next = pied8;
+  table_ronde->next = table_ronde2;
+  table_ronde2->down = plateau3;
+  plateau3->next = pieds3;
+  pieds3->down = pied9;
+  pied9->next = pied10;
+  pied10->next = pied11;
+  pied11->next = pied12;
+  table_ronde2->next = table2;
+  table2->down = plateau4;
+  plateau4->next = pieds4;
+  pieds4->down = pied13;
+  pied13->next = pied14;
+  pied14->next = pied15;
+  pied15->next = pied16;
 }
 
 /* la fonction de contrôle : appelée 1 seule fois, juste après <init> */
