6423589befb7d4f2b38ae716592001d877a75e94
BrayanMa <mariebrayan724@gmail.com>
2022-04-06 17:54:20 +0200

    Ajout du champ dcam

--- src/node.h
+++ src/node.h
@@ -11,6 +11,7 @@ typedef struct _node_
     double mat[4];              /* 4 réels dans [0,1] : (ambi, diff, spec, shine) */
     G3Xvector scale_factor;     /* facteurs d’échelles locaux en x,y,z,*/
     Shape *instance;            /* une éventuelle instance d’objet*/
+    double dcam;                /* distance de la caméra par rapport au node*/
 } Node, *SceneTree;
 
 Node *init_node_with_shape(G3Xhmat Md, G3Xcolor col, double *mat, G3Xvector scale_factor, Shape *instance);
